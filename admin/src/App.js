8f818f993749d03c1e37c42682aaa8480362ad04
shashikumar242 <shashikumar.g.242@gmail.com>
2024-09-01 20:33:11 +0530

    feat: added backend deployed url to frontend

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,8 +3,8 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
-export const currency = '₹';
+export const backend_url = "https://ecommerce-fullstack-1-ap23.onrender.com";
+export const currency = "₹";
 
 function App() {
   return (
